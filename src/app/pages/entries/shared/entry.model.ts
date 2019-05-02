ce3e544a3a90dc0051fb1d439b43cb66a7f845c1
Tra Claude <vroclaude@gmail.com>
2019-05-03 01:56:35 +0200

    setting up EntriesModule

---
+++ src/app/pages/entries/shared/entry.model.ts
@@ -0,0 +1,21 @@
+import {Category} from '../../categories/shared/category.model';
+
+export class  Entry {
+  public id?: number;
+  public name?: string;
+  public description?: string;
+  public type?: string;
+  public amount?: string;
+  public date?: string;
+  public paid?: boolean;
+  public categoryId?: number;
+  public category?: Category;
+static types = {
+  expense: 'expense',
+  revenue: 'revenue'
+};
+get paidText(): string {
+  return this.paid ? 'Paid out' : 'Pending';
+}
+
+}
