abf308c5f7c6f7dae94e80e3544a8df1df977cb5
Tra Claude <vroclaude@gmail.com>
2019-05-08 02:16:21 +0200

    Improving BaseResourceService && refactoring CategoryService

--- src/app/pages/entries/shared/entry.model.ts
+++ src/app/pages/entries/shared/entry.model.ts
@@ -1,5 +1,5 @@
 import {Category} from '../../categories/shared/category.model';
-import {BaseResourceModel} from '../../../shared/models/base-resource-model';
+import {BaseResourceModel} from '../../../shared/models/base-resource.model';
 
 export class Entry  extends BaseResourceModel{
   static types = {
