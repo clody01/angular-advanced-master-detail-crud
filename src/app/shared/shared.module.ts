89175e99bcadc507d1b1dd91971876ac05183c32
Tra Claude <vroclaude@gmail.com>
2019-05-10 14:00:28 +0200

    Adding RouterModule && Improving pages components with BreadCrumbComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 import {ReactiveFormsModule} from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [BreadCrumbComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
+    RouterModule
   ],
   exports: [
     // SHARED MODULES
     CommonModule,
     ReactiveFormsModule,
+    RouterModule,
     // SHARED COMPONENTS
     BreadCrumbComponent
   ]
